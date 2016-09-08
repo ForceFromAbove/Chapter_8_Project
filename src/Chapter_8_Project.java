f7142f9a0a666b49c32f5694019a57e44c1f3059
Aaron Ewing <aaronewing@nnu.edu>
2016-09-08 15:07:21 -0600

    figureing out dis

--- src/Chapter_8_Project.java
+++ src/Chapter_8_Project.java
@@ -1,11 +1,63 @@
-// Chapter 8 Project
+import com.sun.org.apache.xpath.internal.operations.Bool;
 
+import java.util.Scanner;
 
 public class Chapter_8_Project {
+
+    public static void main( String[] args ){           // Test driver
+        Scanner user_Input = new Scanner(System.in);    // Used to get input from user
+
+        String Name;
+        String Address;
+        Long Phone_Number;
+        Float Latitude;
+        Float Longitude;
+        String Nature;
+        Bool Accidental_Call;
+        String Text_Log;
+        String Responding_Units;
+        Bool Status;
+
+        System.out.println("Please enter decimal Number 1:");   // Reads in first double (Number 1)
+        user_Number1 = user_Input.nextDouble();
+
+        System.out.println("Please enter decimal Number 2:");   // Reads in first double (Number 2)
+        user_Number2 = user_Input.nextDouble();
+
+        System.out.printf("Difference: %f%n", difference);
+        System.out.printf("Quotient: %f%n", quotient);
+        try {
+            time.set(x, y, z);
+        }
+        catch (IllegalArgumentException e) {
+            System.out.println("Can you please repeat that?");
+        }
+    }
+
+    private static void class Emergency(int x, contsturctory) {
+
+    }
 }
 
 
-
 /**
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
  * Created by aaronewing on 9/8/2016.
  */
---
+++ src/Emergency.java
@@ -0,0 +1,5 @@
+/**
+ * Created by same guy who  on 9/8/2016.
+ */
+public class Emergency {
+}
